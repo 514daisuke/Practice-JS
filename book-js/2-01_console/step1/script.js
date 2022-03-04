f258ff0d8367e4dc36bbbea1fafdc92d2cc352ea
daisuke.koishi <daisuke.koishi@icloud.com>
2022-03-04 22:31:12 +0900

    first commit

---
+++ book-js/2-01_console/step1/script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+//外部Javascriptファイル
+/*
+外部Javascriptファイルは読み込まれたらすぐに実行される
+*/
+console.log("隣の客は");
---
+++ book-js/practice/2-02_tag/script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+//外部JavaScriptファイル
+/*
+外部のJavaScriptファイルは
+読み込まらたらすぐ実行されます
+*/
+console.log('隣の客は');
